e62f48d80f5de73dff98ef432d96d8d91e1f29db
lixxth <lizbeth78765@gmail.com>
2024-07-03 16:40:34 -0600

    chore: Add PWA support and update image domains in next.config.js and manifest.json

--- next.config.mjs
+++ next.config.mjs
@@ -3,4 +3,19 @@ const nextConfig = {
   reactStrictMode: true,
 };
 
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
+
+module.exports = nextConfig
+// next.config.js
+module.exports = withPWA({
+  images: {
+    domains: ["res.cloudinary.com", "unsplash.com"],
+  },
+});
+
 export default nextConfig;
